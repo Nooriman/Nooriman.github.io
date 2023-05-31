dc0e121003a6a8ef5e8a451c165f9e802a30149e
Nooriman Noor <nooriman.mdnoor@gmail.com>
2023-05-31 22:08:44 +0800

    added services page

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,92 @@
 import React from 'react';
 import "./services.css";
+import { BsCheckLg} from 'react-icons/bs';
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className='service'>
+          <div className='service__head'>
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BsCheckLg className='services__list-icon' />
+              <p>Lorem, Ipsum dolor sit amet consectotur elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='services__list-icon' />
+              <p>Lorem, Ipsum dolor sit amet consectotur elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='services__list-icon' />
+              <p>Lorem, Ipsum dolor sit amet consectotur elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='services__list-icon' />
+              <p>Lorem, Ipsum dolor sit amet consectotur elit.</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BsCheckLg className='services__list-icon' />
+              <p>Lorem, Ipsum dolor sit amet consectotur elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='services__list-icon' />
+              <p>Lorem, Ipsum dolor sit amet consectotur elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='services__list-icon' />
+              <p>Lorem, Ipsum dolor sit amet consectotur elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='services__list-icon' />
+              <p>Lorem, Ipsum dolor sit amet consectotur elit.</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BsCheckLg className='services__list-icon' />
+              <p>Lorem, Ipsum dolor sit amet consectotur elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='services__list-icon' />
+              <p>Lorem, Ipsum dolor sit amet consectotur elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='services__list-icon' />
+              <p>Lorem, Ipsum dolor sit amet consectotur elit.</p>
+            </li>
+            <li>
+              <BsCheckLg className='services__list-icon' />
+              <p>Lorem, Ipsum dolor sit amet consectotur elit.</p>
+            </li>
+          </ul>
+        </article>
+
+        
+      </div>
+    </section>
   )
 }
 
