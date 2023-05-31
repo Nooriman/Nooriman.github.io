e3200ca25e987fe40ccf41bef94a70ff751e1680
Nooriman Noor <nooriman.mdnoor@gmail.com>
2023-05-31 22:07:44 +0800

    added contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './contact.css';
+import { MdOutlineMail } from 'react-icons/md';
+import { RiMessengerLine } from 'react-icons/ri';
+import { BsWhatsapp } from 'react-icons/bs';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_piip7qt', 'template_rsng7mr', form.current, 'RWYS8zP5f3SiP1eBU');
+
+    e.target.reset();
+  }
+
   return (
-    <div>Contact</div>
+    <section id="contact">
+      <h5>Get in Touch!</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMail />
+            <h4>Email</h4>
+            <h5>nooriman.mdnoor@gmail.com</h5>
+            <a href='mailto:nooriman.mdnoor@gmail.com' target='_blank'>Send a message</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine />
+            <h4>Messenger</h4>
+            <h5>Nooriman Noor</h5>
+            <a href='https://m.me/BoogeyDLuffy' target='_blank'>Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp />
+            <h4>WhatsApp</h4>
+            <h5>+65 81830414</h5>
+            <a href='https://api.whatsapp.com/send?phone+6581830414' target='_blank'>Send a message</a>
+          </article>
+        </div>
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your Full Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea name='message' rows={7} placeholder='Your Message' required />
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
