06312ce47ef5602d142220de294f39a660b5ebdf
Nooriman Noor <nooriman.mdnoor@gmail.com>
2023-06-11 15:02:59 +0800

    update portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,51 +1,45 @@
 import React from 'react';
 import "./portfolio.css";
-import IMG1 from '../../assets/portfolio1.jpg';
-import IMG2 from '../../assets/portfolio2.jpg';
-import IMG3 from '../../assets/portfolio3.jpg';
-import IMG4 from '../../assets/portfolio4.jpg';
-import IMG5 from '../../assets/portfolio5.png';
-import IMG6 from '../../assets/portfolio6.jpg';
 
 const data = [
   {
     id: 1,
-    image: IMG1,
+    // image: IMG1,
     title: 'Crypto Currency Dashboard & Financial Visualisation',
     github: 'https://github.com',
     demo: 'https://github.com'
   },
   {
     id: 2,
-    image: IMG2,
+    // image: IMG2,
     title: 'Crypto Currency Dashboard & Financial Visualisation',
     github: 'https://github.com',
     demo: 'https://github.com'
   },
   {
     id: 3,
-    image: IMG3,
+    // image: IMG3,
     title: 'Crypto Currency Dashboard & Financial Visualisation',
     github: 'https://github.com',
     demo: 'https://github.com'
   },
   {
     id: 4,
-    image: IMG4,
+    // image: IMG4,
     title: 'Crypto Currency Dashboard & Financial Visualisation',
     github: 'https://github.com',
     demo: 'https://github.com'
   },
   {
     id: 5,
-    image: IMG5,
+    // image: IMG5,
     title: 'Crypto Currency Dashboard & Financial Visualisation',
     github: 'https://github.com',
     demo: 'https://github.com'
   },
   {
     id: 6,
-    image: IMG6,
+    // image: IMG6,
     title: 'Crypto Currency Dashboard & Financial Visualisation',
     github: 'https://github.com',
     demo: 'https://github.com'
@@ -57,10 +51,11 @@ const Portfolio = () => {
     <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portforlio</h2>
+      
+      <h4 className='coming-soon'>Coming Soon...</h4>
 
       <div className="container portfolio__container">
-
-        {data.map(({ id, image, title, github, demo }) =>
+        {/* {data.map(({ id, image, title, github, demo }) =>
           <article className="portfolio__item" key={id}>
             <div className="portfolio__item-image">
               <img src={image} alt='img1' />
@@ -71,7 +66,7 @@ const Portfolio = () => {
               <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
             </div>
           </article>
-        )}
+        )} */}
 
       </div>
     </section>
