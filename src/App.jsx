6164a8c6cd127d5b7ead99dcdaed7be99f6b2a28
Nooriman Noor <nooriman.mdnoor@gmail.com>
2023-06-11 15:04:09 +0800

    Update main App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css';
-import About from './components/about/About';
-import Contact from './components/contact/Contact';
-import Experience from './components/experience/Experience';
-import Footer from './components/footer/Footer';
-import Header from './components/header/Header';
-import Nav from './components/nav/Nav';
-import Portfolio from './components/portfolio/Portfolio';
-import Services from './components/services/Services';
-import Testimonial from './components/testimonial/Testimonial';
+import "./App.css";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Experience from "./components/experience/Experience";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import Portfolio from "./components/portfolio/Portfolio";
+import Services from "./components/services/Services";
+// import Testimonial from './components/testimonial/Testimonial';
 
 function App() {
   return (
@@ -18,7 +18,6 @@ function App() {
       <Experience />
       <Services />
       <Portfolio />
-      <Testimonial />
       <Contact />
       <Footer />
     </>
